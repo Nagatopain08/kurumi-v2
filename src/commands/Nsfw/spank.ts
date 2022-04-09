a758b33016d4709ccfda08ba0a755792550b8dc7
Examinati <Aliwadood561@gmail.com>
2022-04-09 08:55:26 +0500

    main

--- src/commands/Nsfw/spank.ts
+++ src/commands/Nsfw/spank.ts
@@ -1,44 +1,111 @@
-/** @format */
+<div align="center">
 
-import MessageHandler from "../../Handlers/MessageHandler";
-import BaseCommand from "../../lib/BaseCommand";
-import WAClient from "../../lib/WAClient";
-import { ISimplifiedMessage } from "../../typings";
-import { MessageType, Mimetype } from "@adiwajshing/baileys";
+<a href='https://www.linkpicture.com/view.php?img=LPic624ee0719082f1592522799'><img src='https://www.linkpicture.com/q/WhatsApp-Image-2022-04-07-at-5.59.37-PM.jpeg' type='image'></a>
 
-export default class Command extends BaseCommand {
-	constructor(client: WAClient, handler: MessageHandler) {
-		super(client, handler, {
-			command: "spank",
-			description: `Know it yourself.`,
-			aliases: ["spank"],
-			category: "nsfw",
-			usage: `${client.config.prefix}spank`,
-			baseXp: 50,
-		});
-	}
 
-	run = async (M: ISimplifiedMessage): Promise<void> => {
-		let caption = "";
-		caption += `🌟`;
-		if (!(await this.client.getGroupData(M.from)).nsfw)
-			return void M.reply(
-				`Don't be a pervert, Baka! This is not an NSFW group.`
-			);
-		M.reply(
-			await this.client.util.GIFBufferToVideoBuffer(
-				await this.client.getBuffer(
-					(
-						await this.client.fetch<{ url: string }>(
-							`https://nekos.life/api/v2/img/spank`
-						)
-					).url
-				)
-			),
-			MessageType.video,
-			Mimetype.gif,
-			[caption],
-			caption
-		);
-	};
-}
+
+# **🅺🆄🆁🆄🅼🅸 🆃🅾🅺🅸🆂🅰🅺🅸: A WhatsApp BOT WITH RICH CREATION FEATURES**
+
+
+> A Fully Modular and Whatsapp Efficient Bot <br>
+
+ Button : If you are deploying normally
+
+
+
+[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy?template=https://github.com/Eximinati/tokisaki-kurumi)
+
+
+Button : If you are deploying and using Railway
+
+
+
+[![Deploy on Railway](https://railway.app/button.svg)](https://railway.app/new/template?template=https%3A%2F%2Fgithub.com%2FIssa2001%2FAsunaBotto%2F&envs=PREFIX%2CSESSION%2CMODS%2CCRON%2CMONGO_URI%2CGOOGLE_API_KEY%2CCHAT_BOT_URL&optionalEnvs=MODS%2CCRON%2CGOOGLE_API_KEY%2CCHAT_BOT_URL&PREFIXDesc=Prefix+of+your+bot&SESSIONDesc=A+string+for+the+session+to+be+classified+and+to+get+access+to+the+server+endpoints&MODSDesc=The+phone+numbers+of+the+users+who+you+want+to+be+admin+for+the+bot+%28should+be+in+international+format+without+%2B+and+multiple+numbers+must+be+separated+by+a+comma+%5C%22%2C%5C%22%29&CRONDesc=Cron+string+to+clear+chats+in+a+cycle&MONGO_URIDesc=A+secret+String+for+Mongodb+Connection+%28Required%29&GOOGLE_API_KEYDesc=Google+API+key+for+Google+searches+for+MODS+%28Not+Required%29&CHAT_BOT_URLDesc=BrainShop+API+Brain+url)
+
+</div><br/>
+<br/>
+
+## ✨ Highlights
+## [![WhatsApp Group](https://img.shields.io/badge/WhatsApp-25D366?style=for-the-badge&logo=whatsapp&logoColor=white)](https://chat.whatsapp.com/FXDZOyXwdsK4vuOLPGZKpX) [![TypeScript](https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white)](https://www.typescriptlang.org/) [![NodeJs](https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white)](https://nodejs.org/en/)
+
+
+-   Fully Modular Design
+
+-   Object Oriented
+
+-   Written in [TypeScript](https://www.typescriptlang.org/)
+
+-   Self-Restoring Auth
+
+-   Built with [Baileys](https://github.com/adiwajshing/baileys) (The Best
+
+    WhatsApp Library Out There)
+
+
+
+## 💻 Deploy/Hosting Guide
+## [![WhatsApp Group](https://img.shields.io/badge/WhatsApp-25D366?style=for-the-badge&logo=whatsapp&logoColor=white)](https://chat.whatsapp.com/Gy1JemBGs9wLjVEfzbRJ21) [![TypeScript](https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white)](https://www.typescriptlang.org/) [![NodeJs](https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white)](https://nodejs.org/en/)
+
+
+-   Section Moved to
+
+    [Lumine-Guides](https://github.com/Eximinati/tokisaki-kurumi/blob/main/README.md)
+
+
+
+## 🍥 Features of the Repo
+
+
+
+-   To view all features, click
+
+    here➡️[𝕂𝕦𝕣𝕦𝕞𝕚 𝕗𝕖𝕒𝕥𝕦𝕣𝕖𝕝𝕚𝕤𝕥.𝕞𝕕](https://github.com/Elaina-blob/main/Features.md)
+
+
+
+## 💪 Contribution
+
+
+
+-   Feel free to open issues regarding any problems or if you have any feature requests
+
+-   Make sure to follow the ESLint Rules while editing the code and run
+
+    `yarn run prettier-format` before opening PRs
+
+
+
+## 📄 License
+
+href='https://www.linkpicture.com/view.php?img=LPic62169991d23e711584403'><img src='https://telegra.ph/file/d294e2c082db64f9fcf1f.jpg' type='image'></a>
+Scan this code 
+
+
+
+
+Distributed under the GNU AFFERO GENERAL PUBLIC License. See [LICENSE](/LICENSE)
+
+for more information.
+
+## [![WhatsApp Group](https://img.shields.io/badge/WhatsApp-25D366?style=for-the-badge&logo=whatsapp&logoColor=white)](https://chat.whatsapp.com/Gy1JemBGs9wLjVEfzbRJ21) [![TypeScript](https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white)](https://www.typescriptlang.org/) [![NodeJs](https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white)](https://nodejs.org/en/)
+
+## 🎗Help
+
+Please give this repo a ⭐ if it helped you.
+
+## KURUMi
+
+This is a lovely project by Eximinati. After forking give it a star. 🌟 star it. If you need help then click the following Whatsapp icons and join our groups and contact an OWNER 
+
+ 
+
+HOW TO REACH THE OWNER? 👇👇👇
+
+   https://wa.me/263716564623?text=Hi%20I%20Am%20From%20GitHub%20
+   <a href="https://wa.me/923087880256?text=Hey_Fam%20I%20Am%20From%20GitHub%20☺️">
+   <img scr="https://img.shields.io/badge/WhatsApp-25D366?style=for-the-badge&logo=whatsapp&logoColor=white"/>
+   </a>&nbsp;&nbsp;  
+   <a
+  
+
+   
