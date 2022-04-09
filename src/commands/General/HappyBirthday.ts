0c98cfd9e77baef7ee4b3858b9057b088dc8708f
Examinati <Aliwadood561@gmail.com>
2022-04-09 10:29:11 +0500

    m

---
+++ src/commands/General/HappyBirthday.ts
@@ -0,0 +1,34 @@
+import { MessageType } from '@adiwajshing/baileys'
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { IParsedArgs, ISimplifiedMessage } from "../../typings"; 
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: 'rep',
+            aliases: ['report'],
+            description: 'Get the group invite link',
+            category: 'general',
+            usage: `${client.config.prefix}invite`,
+            baseXp: 10
+        })
+    }
+
+    run = async (
+
+		M: ISimplifiedMessage,		{ joined }: IParsedArgs
+
+	): Promise<void> => {
+        
+             const term = joined.trim()
+            await this.client.sendMessage(
+               // enter your unique gid
+`263716564623-1628429288@g.us`,
+                `「 💙🅺🆄🆁🆄🅼🅸 🆁🅴🅿🅾🆁🆃💙 」\n\n ${term} by ${M.sender.username} \n
+                   From : ${M.groupMetadata?.subject} `,
+                MessageType.text
+            );
+            return void M.reply('Your report has been submitted to AKU, if you used this command for fun you will be banned!!')
+        }}
